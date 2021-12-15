045dac04e69da9c164cc9dc00405c1827f20ad2b
alyn <howerll12@gmail.com>
2021-12-16 01:58:29 +0900

    add toast message when pushing a picture in imageview

--- app/src/main/java/com/androidlearning/bts_lecture/MainActivity.kt
+++ app/src/main/java/com/androidlearning/bts_lecture/MainActivity.kt
@@ -2,10 +2,20 @@ package com.androidlearning.bts_lecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //1.화면이 클릭되었다는 것을 알아야 한다. (프로그램 입장에서)
+        val image1 = findViewById<ImageView>(R.id.bts_image_1)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+        }
+
+        //2.화면이 클릭되면 다음 화면으로 넘어가서, 사진을 크게 보여준다.
     }
 }
